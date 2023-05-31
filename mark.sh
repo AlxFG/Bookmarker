2cbe3f51a1aa432c733dafd9883493cab6bb8dc0
Alx <alexy6725@gmail.com>
2023-05-31 17:52:30 +0100

    refactored the script to not use any C programs and to more gracfully exit when terminated

--- mark.sh
+++ mark.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-buku --np -t | head -n -1 | fzf --reverse | dots | buku --nc -t -f 5 | grep -v 'waiting for input' > /tmp/book; buku -o $(cat /tmp/book | fzf --reverse | number)
+#!/bin/sh
+
+INDEX=$(buku --np -t | head -n -1 | fzf --reverse | sed 's/\..*//')
+if [ "$INDEX" ]; then
+	OUT=$(buku --nc -t -f 5 $INDEX)
+	INDEX=$(printf "%s" "$OUT" | fzf --reverse | sed 's/\t.*//')
+	if [ "$INDEX" ]; then
+		buku -o $INDEX
+	fi
+fi
